bd9d57e6e99e37751127cc761e9e41bf4928444d
Hyun Ku Kim <presentnine@naver.com>
2022-04-15 12:08:06 +0900

    init

---
+++ src/test/java/cf/k5smovie/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.k5smovie.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
